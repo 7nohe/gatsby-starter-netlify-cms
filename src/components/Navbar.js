fb529083b8c09f66ba6075fb375fb8008489219e
7nohe <7nohe.urata@gmail.com>
2018-10-23 22:47:43 +0900

    :sparkles: NavBarスタイル変更

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,12 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
 
 const Navbar = () => (
-  <nav className="navbar is-transparent">
+  <nav className="navbar is-primary">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
+          My Blog
         </Link>
       </div>
       <div className="navbar-start">
@@ -20,18 +16,6 @@ const Navbar = () => (
         <Link className="navbar-item" to="/products">
           Products
         </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
       </div>
     </div>
   </nav>
